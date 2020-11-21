35ee25f70b4339c3e1cbdd11437844498ac7fe65
sgxie <sgxie@vova.com>
2020-11-21 10:44:50 +0800

    patch log

--- common.go
+++ common.go
@@ -10,7 +10,7 @@ import (
 
 func errorExit(err error) {
 	if err != nil {
-		pc, _, _, _ := runtime.Caller(2)
+		pc, _, _, _ := runtime.Caller(1)
 		funcName := runtime.FuncForPC(pc).Name() // 获取函数调用者的名字
 		log.Println("-----------------------------\nError, exit.\n Function: " + funcName + "Error:\n" + error.Error(err))
 		os.Exit(1)
@@ -19,13 +19,13 @@ func errorExit(err error) {
 
 func errorTolerate(err error) {
 	if err != nil {
-		pc, _, _, _ := runtime.Caller(2)
+		pc, _, _, _ := runtime.Caller(1)
 		funcName := runtime.FuncForPC(pc).Name() // 获取函数调用者的名字
 		log.Println("Error, but tolerate it.\n Function: " + funcName + "Error:\n" + error.Error(err))
 	}
 }
 func printLog(smg interface{}) {
-	pc, _, _, _ := runtime.Caller(2)
+	pc, _, _, _ := runtime.Caller(1)
 	funcName := runtime.FuncForPC(pc).Name() // 获取函数调用者的名字
 	log.Println("Function: " + funcName + "  message: \n" + fmt.Sprint(smg))
 }
@@ -33,7 +33,7 @@ func printLog(smg interface{}) {
 // 功能：耗时统计 使用方式：在行数首行执行 defer timeCost()()
 func timeCost() func() {
 	start := time.Now()
-	pc, _, _, _ := runtime.Caller(2)
+	pc, _, _, _ := runtime.Caller(1)
 	funcName := runtime.FuncForPC(pc).Name() // 获取函数调用者的名字
 	return func() {
 		tc := time.Since(start)
